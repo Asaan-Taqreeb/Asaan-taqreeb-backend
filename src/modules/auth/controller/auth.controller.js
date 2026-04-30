85b0678875231b3972ab2c112983c44a621d8423
hiraishaqqqqq-hub <hiraishaqqqqq@gmail.com>
2026-04-30 18:35:35 +0500

    updates

--- src/modules/auth/controller/auth.controller.js
+++ src/modules/auth/controller/auth.controller.js
@@ -8,6 +8,8 @@ const register = async (req, res, next) => {
       return res.status(422).json({ success: false, errors: errors.array() });
     }
 
+
+    
     const result = await authService.register(req.body);
     res.status(201).json({ success: true, data: result });
   } catch (error) {
